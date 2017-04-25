a1c8232b7175fdfa2783511594e60ea9e166c132
valentinatp <valentina.toledoporma@gmail.com>
2017-04-24 21:37:38 -0300

    Añado el archivo de mp3 y añado botones

---
+++ assets/js/javascript.js
@@ -0,0 +1,7 @@
+<audio id="demo" src="assets/musica/where.mp3"></audio>
+<div>
+	<button onclick="document.getElementById('demo').play()">reproducir el audio</button>
+	<button onclick="document.getElementById('demo').pause()">pausar el audio</button>
+	<button onclick="document.getElementById('demo').volumen+=0.1">Aumentar el Volumen</button>
+	<button onclick="document.getElementById('demo').volumen-=0.1">Disminuir el Volumen</button>
+</div>
